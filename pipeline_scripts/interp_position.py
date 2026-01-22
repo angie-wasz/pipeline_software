0c8841586145b9fc37c9d15ff62394a8b785c5f8
angie-wasz <19460541@student.curtin.edu.au>
2026-01-22 14:34:37 +0800

    adding glevel scripts

---
+++ pipeline_scripts/interp_position.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import logging
+
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+from scipy.optimize import minimize
+
+import astropy.units as u
+from astropy.io import fits
+from astropy.table import Table
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+
+parser = argparse.ArgumentParser()
+parser.add_argument('infile', help='Input image')
+parser.add_argument('intable', help='Input table')
+parser.add_argument('outtable', help='Output table')
+parser.add_argument('--ra', default="ra", help="RA Column")
+parser.add_argument('--dec', default="dec", help="Decl. Column")
+parser.add_argument('--peak_flux', default="peak_flux", help="Peak Flux")
+parser.add_argument('--background', default="background", help="Background (not modified)")
+parser.add_argument('--flag_column', default="residual_mean", help="fit only where this column is flagged")
+parser.add_argument('--ofmt', default="votable", help="output file format")
+parser.add_argument('--overwrite', action='store_true', help="overwrite")
+parser.add_argument("--cutoff", dest="cutoff", default=5, help="remove sources with lower S/N")
+parser.add_argument("-v", "--verbose", action="count", dest="verbose", default=0, help="-v info, -vv debug")
+
+args = parser.parse_args()
+
+if args.verbose == 0:
+    logging.basicConfig(format='%(asctime)s-%(levelname)s %(message)s', level=logging.WARNING)
+elif args.verbose == 1:
+    logging.basicConfig(format='%(asctime)s-%(levelname)s %(message)s', level=logging.INFO)
+else:
+    logging.basicConfig(format='%(asctime)s-%(levelname)s %(message)s', level=logging.DEBUG)
+
+root, ext = os.path.splitext(args.infile)
+
+hdus = fits.open(args.infile)
+tab = Table.read(args.intable)
+snr = tab['peak_flux'] / tab['local_rms']
+tab = tab[snr > args.cutoff]
+
+wcs = WCS(hdus[0].header).celestial
+data = hdus[0].data
+
+if not args.overwrite and os.path.exists(args.outtable):
+    raise IOError("outtable exists")
+
+skycoords = SkyCoord(tab['ra'].data.data*u.deg, tab['dec'].data.data*u.deg)
+mask = tab[args.flag_column].mask
+
+xf, yf = wcs.wcs_world2pix(skycoords.ra.deg, skycoords.dec.deg, 0)
+y = np.round(yf).astype(int)
+x = np.round(xf).astype(int)
+
+xx = np.array([-1., 0., 1])
+peak_flux = np.zeros(len(x), dtype=np.float32)
+x_out = np.zeros(len(x), dtype=np.float32)
+y_out = np.zeros(len(x), dtype=np.float32)
+niter = np.zeros(len(x), dtype=int)
+flag = np.zeros(len(x), dtype=int)
+
+vertices = np.array([[-np.sqrt(3)/4, -0.25], [0, 0.5], [np.sqrt(3)/4, -0.25]])
+
+for i in range(len(x)):
+    if ~mask[i]:
+        flag[i] = -1
+        logging.info("%s err:%d -- not masked -- skipping", tab['uuid'][i], flag[i])
+        continue
+    s = (0, 0, slice(y[i]-1, y[i]+2), slice(x[i]-1, x[i]+2))
+    d = data[s]
+    if not d.shape == (3,3):
+        flag[i] = 1
+        logging.warning("%s err:%d -- invalid shape -- skipping", tab['uuid'][i], flag[i])
+        continue
+    interp = RectBivariateSpline(xx,xx,d, kx=2,ky=2)
+    coords = (yf[i]-y[i], xf[i]-x[i])
+    f = lambda x: -interp(x[0], x[1])[0][0]
+    min_ = minimize(fun=f, x0=(0, 0), method='Nelder-Mead', 
+                    options={'xatol': 0.01, 'initial_simplex':vertices})
+    if not min_['success']:
+        flag[i] = 2
+        logging.warning("%s err:%d -- fit failed in %d iterations -- %s", tab['uuid'][i], flag[i], min_['nit'], min_['message'])
+        continue
+    peak_flux[i] = -min_['fun']
+    y_out[i], x_out[i] = min_['x']
+    if np.abs(x_out[i]) > 1 or np.abs(y_out[i]) > 1:
+        flag[i] = 3
+        logging.warning("%s err:%d -- interp position outside pixel at %d,%d. %d iterations -- %s", tab['uuid'][i], flag[i], x_out[i], y_out[i], min_['nit'], min_['message'])
+    else:
+        logging.debug("%s err:%d -- %d iterations -- %s", tab['uuid'][i], flag[i], min_['nit'], min_['message'])
+
+replace = mask & (flag == 0)
+xy = np.stack((x+x_out, y+y_out)).swapaxes(0, 1)
+skycoords2 = wcs.celestial.wcs_pix2world(xy, 0)
+tab[args.peak_flux][:] = np.where(replace, peak_flux-tab[args.background], tab[args.peak_flux][:])
+tab[args.ra][:] = np.where(replace, skycoords2[:, 0], tab[args.ra][:])
+tab[args.dec][:] = np.where(replace, skycoords2[:, 1], tab[args.dec][:])
+tab['pos_interp_flag'] = flag
+tab.write(args.outtable, format=args.ofmt, overwrite=args.overwrite)
---
+++ pipeline_scripts/make_cat_tidy.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import os
+import logging
+import numpy as np
+from astropy.table import Table
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, get_sun
+from imstack import ImageStack
+from scipy.optimize import minimize
+
+
+from optparse import OptionParser, OptionValueError
+
+POL_OPTIONS=("XX", "YY", "I")
+
+def get_ds(peak_flux, background, local_rms):
+	"""
+	dS from IPS Paper I equation
+	"""
+	dS = np.sqrt((peak_flux+background)**2 - background**2)
+	err_dS = np.sqrt((peak_flux+background+local_rms)**2 - background**2) - dS
+	return dS, err_dS
+
+parser = OptionParser(usage = "usage: %prog input.hdf5 input.vot output.vot" +
+"""
+
+add various columns and delete unneeded ones.
+""")
+parser.add_option("-i", "--interp", dest="interp", action="store_true", help="Calculate interp image parameters (dS2, snr_scint etc)")
+parser.add_option("-o", "--obsid", dest="obsid", default=None, help="time in gps format used for calculation of Sun location (default: first 10 letters of input.hdf5)")
+parser.add_option("--pol", dest="pol", default="I", help="primary beam polarisation to use: {} (default=%default)".format((", ".join(POL_OPTIONS))))
+parser.add_option("-m", "--moment2", dest="moment2", action="store_true", help="Calculate variability image parameters (dS etc)")
+parser.add_option("-c", "--cutoff", dest="cutoff", default=5, help="remove sources with lower S/N")
+parser.add_option("-v", "--verbose", action="count", dest="verbose", default=0, help="-l info, -ll debug")
+
+opts, args = parser.parse_args()
+#FIXME add options for 
+# freq
+# ra column name
+# dec column name
+# new column name
+# out table format
+# variability
+# gpstime
+# set verbosity
+if opts.moment2 and opts.interp:
+    raise OptionValueError("-m/--moment2 can not be set with -i/--interp")
+if not opts.pol in POL_OPTIONS:
+    raise OptionValueError("polarisation must be one of %s" % (", ".join(POL_OPTIONS)))
+
+if opts.verbose == 0:
+    logging.basicConfig(format='%(asctime)s-%(levelname)s %(message)s', level=logging.WARNING)
+elif opts.verbose == 1:
+    logging.basicConfig(format='%(asctime)s-%(levelname)s %(message)s', level=logging.INFO)
+else:
+    logging.basicConfig(format='%(asctime)s-%(levelname)s %(message)s', level=logging.DEBUG)
+
+if os.path.exists(args[2]):
+    os.remove(args[2])
+
+imstack = ImageStack(args[0], freq='121-132', image_type=None)
+dim_x, dim_y = imstack.group['beam'].shape[1:3]
+
+t = Table.read(args[1])
+
+# elongation
+if not opts.obsid:
+    opts.obsid = args[0][:10]
+logging.info("calculating elongations")
+time = Time(float(opts.obsid), format='gps')
+sun = get_sun(time.utc)
+t['elongation'] = sun.separation(SkyCoord(t['ra'], t['dec'], unit = "deg"))
+
+t['snr'] = t['peak_flux'] / t['local_rms']
+t = t[t['snr'] > opts.cutoff]
+if opts.interp:
+    t['snr_scint'] = t['peak_flux2'] / t['local_rms2']
+
+logging.info("calculating primary beam max")
+logging.debug("beam at central pixel: %f", imstack.pix2beam(1200, 1200, scale=True))
+f = lambda x: -imstack.pix2beam(int(np.round(x[0])), int(np.round(x[1])), scale=True)
+min_ = minimize(fun=f, x0=(1200.0, 1200.0), method='Nelder-Mead', options={'xatol': 1})
+pbmax = -min_['fun']
+
+logging.info("calculating primary beam for each source")
+t["pbcor"] = np.nan*np.ones(len(t))
+# loop over all unmasked values
+for s in np.argwhere(~t['ra'].mask)[:, 0]:
+    #print(s, sep=' ')
+    x, y = imstack.world2pix(t['ra'][s], t['dec'][s])
+    #print(x,y)
+    if x<0 or x >= dim_x:
+        continue
+    if y<0 or y >= dim_y:
+        continue
+    if opts.pol == "I":
+        t["pbcor"][s] = imstack.pix2beam(x, y, scale=True)
+    elif opts.pol == "XX":
+        t["pbcor"][s] = imstack.pix2beam(x, y, avg_pol=False, scale=True)[0]
+    elif opts.pol == "YY":
+        t["pbcor"][s] = imstack.pix2beam(x, y, avg_pol=False, scale=True)[1]
+t["pbcor_norm"] = t["pbcor"]/pbmax
+
+if opts.moment2:
+    t['dS'], t['err_dS']= get_ds(t['peak_flux'], t['background'], t['local_rms'])
+    t['scint_index'] = t['dS'] / t['peak_flux']
+    t['err_scint_index'] = t['scint_index'] * t['err_dS'] / t['dS']
+    t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'dS', 'err_dS', 'background', 'local_rms', 'snr'])
+elif opts.interp:
+    t['dS2'], t['err_dS2']= get_ds(t['peak_flux2'], t['background2'], t['local_rms2'])
+    t['scint_index2'] = t['dS2'] / t['peak_flux2']
+    t['err_scint_index2'] = t['scint_index2'] * t['err_dS2'] / t['dS2']
+    t.keep_columns(['peak_flux', 'err_peak_flux', 'local_rms', 'snr', 'ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'peak_flux2', 'background2', 'local_rms2', 'dS2', 'err_dS2', 'snr_scint', 'scint_index2', 'err_scint_index2'])
+else:
+    t.keep_columns(['ra', 'err_ra', 'dec', 'err_dec', 'a', 'b', 'pa', 'elongation', 'pbcor', 'pbcor_norm', 'uuid', 'peak_flux', 'background', 'local_rms', 'snr'])
+
+logging.info("writing votable")
+t.write(args[2], format='votable')
