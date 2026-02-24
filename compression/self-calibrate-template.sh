f8eb98e2dac844fe065cc4059a635266c12c514f
angie-wasz <19460541@student.curtin.edu.au>
2026-02-24 15:03:23 +0800

    making self-cal script

--- compression/self-calibrate-template.sh
+++ compression/self-calibrate-template.sh
@@ -43,11 +43,14 @@ date -Iseconds
 
 rsync -av /scratch/mwasci/asvo/{{asvo}}/{{obsid}}.ms/ ./{{obsid}}.ms
 
+sky_model=sun_model_{{obsid}}.yaml
+
 hyperdrive di-calibrate -d {{obsid}}.ms {{obsid}}.metafits \
 	-s {{skymodel}} \
 	--num-sources {{sources}} \
 	--uvw-min {{uvwmin}} \
 	--uvw-max {{uvwmax}} \
+	--tile-flags Tile051 Tile052 Tile053 Tile054 Tile055 Tile056 Tile057 Tile058 Tile071 Tile072 Tile073 Tile074 Tile075 Tile076 Tile077 Tile078 Tile97 Tile98 Tile101 Tile102 Tile103 Tile104 Tile105 Tile106 Tile107 Tile108 Tile111 Tile112 Tile113 Tile114 Tile115 Tile116 Tile117 Tile118 Tile121 Tile122 Tile123 Tile124 Tile125 Tile126 Tile127 Tile128 Tile131 Tile132 Tile133 Tile134 Tile135 Tile136 Tile137 Tile138 Tile141 Tile142 Tile143 Tile144 Tile145 Tile146 Tile147 Tile148 Tile151 Tile152 Tile153 Tile154 Tile155 Tile156 Tile157 Tile158 Tile161 Tile162 Tile163 Tile164 Tile165 Tile166 Tile167 Tile168 \
 	-o {{obsid}}_sols.fits
 
 hyperdrive plot-solutions {{obsid}}_sols.fits
