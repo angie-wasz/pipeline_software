960e535c7ac543ece4c2747a925433957f3e8792
angie-wasz <19460541@student.curtin.edu.au>
2026-05-11 13:34:58 +0800

    cleaning up comments

--- new_system/run.sh
+++ new_system/run.sh
@@ -58,7 +58,8 @@ fi
 sleep $(echo "scale=3; $RANDOM/32768*10" | bc)
 
 SOFTWARE=/software/projects/mwasci/awaszewski/new_system/
-SCRATCH=/scratch/mwasci/awaszewski/pipeline
+SCRATCH=/scratch/mwasci/awaszewski/pipeline/
+ASVO=/scratch/mwasci/asvo/
 
 # Data storage
 echo "${OBSID} Checking if data directory already exists"
@@ -67,23 +68,26 @@ if [ ! -d ${DATA} ]; then
 	echo "${OBSID} creating data directory"
 	mkdir ${DATA}
 fi
-# I'm aware that if a data directory already exists, its probably been processed previously but easier to check with log
 
 ASVO_SKIP=FALSE
 CAL_SKIP=FALSE
 
+# I'm aware that if a data directory already exists, its probably been processed previously but easier to check with log
 # Start log
 echo "${OBSID} Checking if observation is initialised in log"
 output=$(python read_log.py -l ${LOG} -o ${OBSID})
 
 if [ -z "$output" ]; then
+	# If not previously processed, add it to the log
 	echo "${OBSID} initialising observation in log"
 	python update_log.py -l ${LOG} -o ${OBSID} --initialise
 else
+	# Otherwise, read info about the observation
 	stage=$(echo ${output} | cut -d "|" -f 3 | awk '{print $2}')
 	status=$(echo ${output} | cut -d "|" -f 4 | awk '{print $2}')
 	ASVOID=$(echo ${output} | cut -d "|" -f 2 | awk '{print $2}')
 
+	# Has it failed in the past?
 	if [[ $SKIP_FAIL_CHECK = FALSE ]]; then 
 		if [[ "$status" == "Failed" ]]; then
 			echo "${OBSID} has been processed in the past and has failed. Skipping this observation. If you want to run it again, then create new list of failed observations and add flag --skip-fail-check"
@@ -91,9 +95,10 @@ else
 		fi
 	fi
 	
+	# Is data from ASVO already available?
 	if [[ $ASVOID -ne 0 ]]; then
 		echo "${OBSID} has already been downloaded from ASVO. Checking if measurement set is available."
-		ms=/scratch/mwasci/asvo/${ASVOID}/${OBSID}_ch121-132.ms/table.dat
+		ms=${ASVO}/${ASVOID}/${OBSID}_ch121-132.ms/table.dat
 		if [ -f ${ms} ]; then
 			echo "${OBSID} measurement set available. Skipping ASVO download"
 			ASVO_SKIP=TRUE
@@ -102,6 +107,7 @@ else
 		fi
 	fi
 		
+	# Has it been calibrated?
 	cal_sols=${DATA}/${OBSID}_sols_162MHz_160.bin
 	if [ -f ${cal_sols} ]; then
 		echo "${OBSID} calibration solutions are available. Skipping calibration"
@@ -112,23 +118,26 @@ else
 
 fi
 
-# ASVO staging
+# ASVO staging (if required)
 if [ $ASVO_SKIP = FALSE ]; then
 	bash ./asvo.sh ${OBSID} ${LOG}
 	ASVOID=$(python read_log.py -l ${LOG} -o ${OBSID} | cut -d "|" -f 2 | awk '{print $2}')
 fi
 
-# Calibration
+# Calibration (if required)
 if [ $CAL_SKIP = FALSE ]; then
 	bash ./calibrate.sh ${OBSID} ${ASVOID} ${DATA} ${SOFTWARE} ${LOG}
 fi
 
 # Data Quality
 echo "${OBSID} Checking data quality"
+
 module load singularity/4.1.0-slurm
 module unload python/3.11.6 py-numpy/1.25.2 py-astropy/4.2.1
+
 container="/software/projects/mwasci/awaszewski/ips_post.img"
 singularity exec -B $PWD ${container} python /software/projects/mwasci/awaszewski/quality_scripts/calc_quality.py -o ${OBSID} -p ${DATA} -l ${SOFTWARE}/${LOG} -s ${SOFTWARE}
+
 module load python/3.11.6 py-numpy/1.25.2 py-astropy/4.2.1 
 
 frac_bad=$(python read_log.py -l ${LOG} -o ${OBSID} --quality | cut -d "|" -f 2 | awk '{print $2}')
@@ -140,6 +149,7 @@ if [[ -z "$frac_bad" || -z "$resid" ]]; then
 	exit 1
 fi
 
+# Does it qualify for imaging?
 frac_bad_limit=0.6
 resid_limit=20
 if (( $(echo "$frac_bad > $frac_bad_limit" | bc -l) )); then
@@ -150,6 +160,7 @@ if (( $(echo "$frac_bad > $frac_bad_limit" | bc -l) )); then
 	fi
 fi
 
+# Stopping here if only calibration option was chosen
 if [ "$CALIBRATE" == "TRUE" ]; then
 	echo "${OBSID} As only calibration was chosen, workflow finishing now"
 	exit
@@ -174,21 +185,25 @@ else
 	STAGE="full"
 fi
 
+# Some form of imaging must run if it is selected
 DATA="/scratch/mwasci/awaszewski/pipeline/${OBSID}/"
 bash ./image.sh ${STAGE} ${OBSID} ${ASVOID} ${cal_sols} ${DATA} ${SOFTWARE} ${LOG}
 
+# Did it successfully image?
 output=$(python read_log.py -l ${LOG} -o ${OBSID})
 status=$(echo ${output} | cut -d "|" -f 4 | awk '{print $2}')
 if [[ "$status" == "Failed" ]]; then
-	echo "${OBSID} has failed image or post-image, exitting pipeline"
+	echo "${OBSID} has failed image or post-image, exiting pipeline"
 	exit
 fi
 
-FREQ=121-132
 # g-level and Acacia storage
-#if [[ ("$STAGE" == "full") || ("$STAGE" == "post") ]]; then
+# If post image was run, then do post processing
+#FREQ='121-132'
+if [[ ("$STAGE" == "full") || ("$STAGE" == "post") ]]; then
+# CURRENTLY HAVE ISSUES WITH THIS PART, WEIRD CONTAINER ISSUES
 #	bash ./glevel.sh ${OBSID} ${DATA} ${SOFTWARE} ${LOG} ${FREQ}
-#	bash ./acacia.sh ${OBSID} ${SCRATCH} ${SOFTWARE}
-#fi
+	bash ./acacia.sh ${OBSID} ${SCRATCH} ${SOFTWARE}
+fi
 
 echo "${OBSID} done"
